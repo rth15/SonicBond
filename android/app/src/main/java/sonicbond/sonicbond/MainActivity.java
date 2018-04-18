166bc6286054ecfe9b63233ad25064de90a8c2bc
rth15 <rherzfeldt@gmail.com>
2018-04-18 18:51:11 -0400

    All Project Files Pushed

--- android/app/src/main/java/sonicbond/sonicbond/MainActivity.java
+++ android/app/src/main/java/sonicbond/sonicbond/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 /*
 Summary: UIManager & MainActivity Module for Sonic Bond
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private AudioManager mAudioManager;
 
     private Button mBtnSettings;
+    private Switch switchMute;
+    private Switch switchVibration;
     private SeekBar volumeSeekBar = null;
 
     private Button mBtnStart;
@@ -50,19 +53,67 @@ public class MainActivity extends AppCompatActivity {
         volumeSeekBar.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
         volumeSeekBar.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
 
+        switchMute = findViewById(R.id.switchMute);
+        if (mAudioManager.getRingerMode() == mAudioManager.RINGER_MODE_SILENT)
+            switchMute.setChecked(true);
+        else
+            switchMute.setChecked(false);
+
+        switchMute.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if (switchMute.isChecked()) {
+                    switchMute.setChecked(false);
+                }
+                else{
+                    switchMute.setChecked(true);
+                }
+            }
+        });
+
+        switchVibration = findViewById(R.id.switchVibration);
+        if (mAudioManager.getRingerMode() == mAudioManager.RINGER_MODE_VIBRATE)
+            switchVibration.setChecked(true);
+        else
+            switchVibration.setChecked(false);
+
+        switchMute.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                if (switchVibration.isChecked()) {
+                    switchVibration.setChecked(false);
+                }
+                else{
+                    switchVibration.setChecked(true);
+                }
+            }
+        });
+
+
         mBtnSettings = findViewById(R.id.btnSettings);
         mBtnSettings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
+
+                volumeSeekBar.setProgress(mAudioManager.getStreamVolume(AudioManager.STREAM_ALARM));
+
                 if (volumeSeekBar.getVisibility() == View.INVISIBLE){
                     volumeSeekBar.setActivated(true);
-                    volumeSeekBar.setVisibility(View.VISIBLE);}
+                    volumeSeekBar.setVisibility(View.VISIBLE);
+                    switchMute.setVisibility(View.VISIBLE);
+                    switchVibration.setVisibility(View.VISIBLE);
+                }
                 else{
                     volumeSeekBar.setActivated(false);
                     volumeSeekBar.setVisibility(View.INVISIBLE);}
                     mAudioManager.setStreamVolume(AudioManager.STREAM_ALARM,
                         volumeSeekBar.getProgress(), 0);
-            }
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_RING,
+                        volumeSeekBar.getProgress(), 0);
+
+                    switchMute.setVisibility(View.INVISIBLE);
+                    switchVibration.setVisibility(View.INVISIBLE);
+                }
         });
+
+
 
 
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
